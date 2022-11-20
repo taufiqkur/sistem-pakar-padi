eb83827e3779c0d9f6395b82d7919d52149b9837
unknown <191210066@student.mercubuana-yogya.ac.id>
2022-11-20 18:57:39 +0700

    Commit Pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SISTEM PAKAR</title>
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b2fefa;">
+        <a class="navbar-brand d-lg-none" href="#">Coding Yaar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler2"
+            aria-controls="myNavbarToggler2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="myNavbarToggler2">
+            <ul class="navbar-nav mx-auto">
+                <!--<a class="navbar-brand d-none d-lg-block" href="#">Coding Yaar</a>-->
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tips</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="d-block mx-auto" style="width: 1000px; border: solid; height: 350px;">
+            <h1 class="text-center">INI GAMBAR TUMBNAIL</h1>
+        </div>
+    </div>
+</div>
+
+    <button class="btn btn-primary d-block mx-auto mt-5">Mulai Sekarang</button>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <!--navbar-->
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b2fefa;">
+    <a class="navbar-brand d-lg-none" href="#">Coding Yaar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbarToggler2"
+        aria-controls="myNavbarToggler2" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="myNavbarToggler2">
+        <ul class="navbar-nav mx-auto">
+            <!--<a class="navbar-brand d-none d-lg-block" href="#">Coding Yaar</a>-->
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tips</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--gambar-->
+<div class="container-fluid py-5">
+    <div class="container">
+        <div class="d-block mx-auto" style="width: 1000px">
+            <img src="resources/views/poster.jpeg" alt="">
+        </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
